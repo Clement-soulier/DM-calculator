c6cd78e880fc19e1c2e7e9bbd85ac291e829d6bd
Soulier Clément <clement.soulier12@gmail.com>
2023-03-28 08:33:54 +0200

    -Added some constants in javascript

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//variables
 let currentOperand = "";
 let previousOperand = "";
 let operation = undefined;
 let computation = "";
 
+//constants
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelectorAll("[data-equals]");
@@ -12,6 +14,29 @@ const previousOperandTextElement = document.querySelectorAll(
 );
 const currentOperandTextElement = document.querySelectorAll(
   "[data-current-operand]"
+);
+
+//assertion to verify constants
+console.assert(
+  numberButtons.length == 10,
+  "numberButtons.length must equal to 10"
+);
+console.assert(
+  operationButtons.length == 4,
+  "operationButtons.length must equal to 4"
+);
+console.assert(equalsButton.length == 1, "equalsButton.length must equal to 1");
+console.assert(
+  allClearButton.length == 2,
+  "allClearButton.length must equal to 2"
+);
+console.assert(
+  previousOperandTextElement.length == 1,
+  "previousOperandTextElement.length must equal to 1"
+);
+console.assert(
+  currentOperandTextElement.length == 1,
+  "currentOperandTextElement.length must equal to 1"
 );
 
 function clear() {
