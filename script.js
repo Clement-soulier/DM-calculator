23b0d7f543a445b3bd2a7875fe6732bc602b3d6a
Soulier Clément <clement.soulier12@gmail.com>
2023-03-28 08:32:46 +0200

    -Modiffied HTML to add attributes to buttons -Added some constants in javascript

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ let previousOperand = "";
 let operation = undefined;
 let computation = "";
 
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelectorAll("[data-equals]");
+const allClearButton = document.querySelectorAll("[data-all-clear]");
+const previousOperandTextElement = document.querySelectorAll(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelectorAll(
+  "[data-current-operand]"
+);
+
 function clear() {
   /**
    * Clear the currentOperand,   previousOperand,  operation variable
